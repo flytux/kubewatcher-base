467966eabe80fc39283a499743713db84e164923
musicauxxx <and2019**>
2020-11-23 08:47:00 +0900

    cluster-Access Control 상세화면 작업

--- src/main/java/com/kubeworks/watcher/ecosystem/kubernetes/service/RoleBindingService.java
+++ src/main/java/com/kubeworks/watcher/ecosystem/kubernetes/service/RoleBindingService.java
@@ -1,13 +1,16 @@
 package com.kubeworks.watcher.ecosystem.kubernetes.service;
 
-import com.kubeworks.watcher.ecosystem.kubernetes.dto.EndpointTable;
+import com.kubeworks.watcher.ecosystem.kubernetes.dto.RoleBindingDescribe;
 import com.kubeworks.watcher.ecosystem.kubernetes.dto.RoleBindingTable;
 import lombok.SneakyThrows;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RoleBindingService {
 
     @SneakyThrows
     List<RoleBindingTable> allNamespaceRoleBindingTables();
+
+    Optional<RoleBindingDescribe> roleBinding(String namespace, String name);
 }
--- src/main/java/com/kubeworks/watcher/ecosystem/kubernetes/service/ServiceAccountService.java
+++ src/main/java/com/kubeworks/watcher/ecosystem/kubernetes/service/ServiceAccountService.java
@@ -1,13 +1,17 @@
 package com.kubeworks.watcher.ecosystem.kubernetes.service;
 
+import com.kubeworks.watcher.ecosystem.kubernetes.dto.ServiceAccountDescribe;
 import com.kubeworks.watcher.ecosystem.kubernetes.dto.ServiceAccountTable;
 import lombok.SneakyThrows;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ServiceAccountService {
 
     @SneakyThrows
     List<ServiceAccountTable> allNamespaceServiceAccountTables();
+
+    Optional<ServiceAccountDescribe> serviceAccount(String namespace, String name);
 
 }
