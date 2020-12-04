f9bdd434ae2481138e5a83fd90cbfb8db5133c61
musicauxxx <and2019**>
2020-12-04 11:23:55 +0900

    V1ObjectTableList 변경 (name)

--- src/main/java/com/kubeworks/watcher/ecosystem/kubernetes/dto/crd/StorageV1StorageClassTableList.java
+++ src/main/java/com/kubeworks/watcher/ecosystem/kubernetes/dto/crd/StorageV1StorageClassTableList.java
@@ -1,16 +1,23 @@
 package com.kubeworks.watcher.ecosystem.kubernetes.dto.crd;
 
 import com.kubeworks.watcher.ecosystem.kubernetes.dto.StorageClassTable;
-import com.kubeworks.watcher.ecosystem.kubernetes.dto.crd.base.V1CellTableList;
+import com.kubeworks.watcher.ecosystem.kubernetes.dto.crd.base.V1ObjectAsTable;
+import com.kubeworks.watcher.ecosystem.kubernetes.dto.crd.base.V1ObjectTableList;
+import io.kubernetes.client.openapi.models.V1StorageClass;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
 @Getter
 @Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class StorageV1StorageClassTableList extends V1CellTableList<StorageClassTable> {
+public class StorageV1StorageClassTableList extends V1ObjectTableList<StorageClassTable, V1StorageClass> {
 
     @Override
     protected StorageClassTable getDataObject() {
@@ -42,4 +49,29 @@ public class StorageV1StorageClassTableList extends V1CellTableList<StorageClass
                 break;
         }
     }
+
+    @Override
+    public List<StorageClassTable> getDataTable() {
+        if (super.getRows() == null || super.getColumnDefinitions() == null) {
+            return Collections.emptyList();
+        }
+
+        List<StorageClassTable> list = new ArrayList<>(super.getRows().size());
+        for (V1ObjectAsTable<V1StorageClass> row : super.getRows()) {
+            StorageClassTable data = getDataObject();
+            final List<String> cells = row.getCells();
+            IntStream.range(0, cells.size()).forEach(index -> {
+                String value = cells.get(index);
+                V1ObjectColumnDefinition columnDefinition = super.getColumnDefinitions().get(index);
+                String fieldName = columnDefinition.getName().toLowerCase();
+                makeObject(data, fieldName, value);
+            });
+            if (row.getObject().getMetadata() != null) {
+                data.setName(row.getObject().getMetadata().getName());
+            }
+            list.add(data);
+        }
+        return list;
+    }
+
 }
