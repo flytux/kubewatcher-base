5e1243d337b28603db3441c05bdea24510e4727e
songbaek.jo <songbaek.jo@kubeworks.net>
2021-01-12 17:28:58 +0900

    1. schema.sql 현행화

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,86 +1,94 @@
--- create database `kube-watcher`;
--- create user 'kwuser'@'%' identified by 'kubeworks!@';
--- create user 'kwuser'@'localhost' identified by 'kubeworks!@';
---
--- grant all privileges on `kube-watcher`.* to kwuser@'%';
--- grant all privileges on `kube-watcher`.* to kwuser@'localhost';
--- flush privileges;
--- drop table page, page_row, page_row_panel, chart_query, page_variable, variable_query;
+-- `kube-watcher`.page definition
 
-create table page
-(
-    page_id        bigint unsigned auto_increment      primary key,
-    title          varchar(200)                     not null,
-    nav            varchar(1000)                    not null,
-    fragment_name  varchar(50)                      null,
-    create_time    timestamp default CURRENT_TIMESTAMP not null,
-    update_time    timestamp default CURRENT_TIMESTAMP not null,
-    description    varchar(500)                        null,
-    menu_id        bigint unsigned                     not null
-);
+CREATE TABLE `page` (
+    `page_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+    `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `description` varchar(500) DEFAULT NULL,
+    `menu_id` bigint unsigned NOT NULL,
+    `nav` varchar(1000) NOT NULL,
+    `title` varchar(200) NOT NULL,
+    PRIMARY KEY (`page_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table page_row
-(
-    page_row_id bigint unsigned auto_increment
-        primary key,
-    title       varchar(200)                          not null,
-    sort_order  bigint unsigned                       not null,
-    row_type    varchar(10)                           not null,
-    create_time timestamp   default CURRENT_TIMESTAMP not null,
-    update_time timestamp   default CURRENT_TIMESTAMP not null,
-    page_id     bigint unsigned                       null,
-    constraint PAGE_ROW_FK01
-        foreign key (page_id) references page (page_id)
-);
 
-create table page_row_panel
-(
-    panel_id    bigint unsigned auto_increment
-        primary key,
-    title       varchar(200)                              not null,
-    sort_order  bigint unsigned                           not null,
-    panel_type  varchar(20)                               not null,
-    chart_type  varchar(20)                               not null,
-    refresh_interval  bigint unsigned       default 60000    not null,
-    yaxis_label varchar(50)                               not null,
-    yaxis_unit  varchar(20)                               not null,
-    yaxis_min   varchar(10)                               not null,
-    yaxis_max   varchar(10)                               not null,
-    xaxis_mode  varchar(20)                               not null,
-    create_time timestamp       default CURRENT_TIMESTAMP not null,
-    update_time timestamp       default CURRENT_TIMESTAMP not null,
-    page_row_id bigint unsigned                           null,
-    constraint PAGE_ROW_PANEL_FK01
-        foreign key (page_row_id) references page_row (page_row_id)
-);
+-- `kube-watcher`.page_row definition
 
-create table page_variable
-(
-    variable_id bigint unsigned auto_increment                    primary key,
-    name                varchar(100)                              not null,
-    sort_order          bigint unsigned                           not null,
-    edge_fields         varchar (100)                             null,
-    variable_type       varchar(20)                               not null,
-    refresh_interval    varchar(3)                                not null,
-    hidden_yn           varchar(1)                                not null,
-    job_name            varchar(50)                               not null,
-    api_query           varchar(1000)                             not null,
-    create_time     timestamp           default CURRENT_TIMESTAMP not null,
-    update_time     timestamp           default CURRENT_TIMESTAMP not null,
-    page_id     bigint unsigned                                   null,
-    constraint PAGE_VARIABLE_FK01
-        foreign key (page_id) references page (page_id)
-);
+CREATE TABLE `page_row` (
+    `page_row_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+    `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `row_type` varchar(10) NOT NULL,
+    `sort_order` bigint unsigned NOT NULL,
+    `title` varchar(200) NOT NULL,
+    `page_id` bigint unsigned DEFAULT NULL,
+    PRIMARY KEY (`page_row_id`),
+    KEY `PAGE_ROW_FK01` (`page_id`),
+    CONSTRAINT `PAGE_ROW_FK01` FOREIGN KEY (`page_id`) REFERENCES `page` (`page_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table chart_query
-(
-    c_query_id bigint unsigned auto_increment                    primary key,
-    api_query   varchar(1000)                                    not null,
-    legend      varchar(100)                                     not null,
-    query_step  varchar(3)                                       not null,
-    create_time     timestamp          default CURRENT_TIMESTAMP not null,
-    update_time     timestamp          default CURRENT_TIMESTAMP not null,
-    panel_id    bigint unsigned                                  null,
-    constraint CHART_QUERY_FK01
-        foreign key (panel_id) references page_row_panel (panel_id)
-);
+
+-- `kube-watcher`.page_row_panel definition
+
+CREATE TABLE `page_row_panel` (
+      `panel_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+      `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+      `chart_type` varchar(20) NOT NULL,
+      `fragment_name` varchar(50) DEFAULT NULL,
+      `legend_visible` tinyint(1) NOT NULL DEFAULT '1',
+      `main_yn` varchar(1) NOT NULL DEFAULT 'Y',
+      `panel_type` varchar(20) NOT NULL,
+      `refresh_interval` bigint unsigned NOT NULL DEFAULT '60000',
+      `repeat_variable` varchar(50) DEFAULT NULL,
+      `sort_order` bigint unsigned NOT NULL DEFAULT '0',
+      `title` varchar(200) NOT NULL,
+      `xaxis_mode` varchar(20) NOT NULL,
+      `yaxis_label` varchar(50) NOT NULL,
+      `yaxis_max` varchar(10) NOT NULL,
+      `yaxis_min` varchar(10) NOT NULL,
+      `yaxis_unit` varchar(20) NOT NULL,
+      `page_row_id` bigint unsigned DEFAULT NULL,
+      PRIMARY KEY (`panel_id`),
+      KEY `PAGE_ROW_PANEL_FK01` (`page_row_id`),
+      CONSTRAINT `PAGE_ROW_PANEL_FK01` FOREIGN KEY (`page_row_id`) REFERENCES `page_row` (`page_row_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- `kube-watcher`.page_variable definition
+
+CREATE TABLE `page_variable` (
+    `variable_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+    `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `api_query` varchar(1000) NOT NULL,
+    `edge_fields` varchar(100) DEFAULT NULL,
+    `hidden_yn` varchar(1) NOT NULL,
+    `job_name` varchar(50) NOT NULL,
+    `name` varchar(100) NOT NULL,
+    `query_type` varchar(20) NOT NULL,
+    `refresh_interval` varchar(3) NOT NULL,
+    `sort_order` bigint unsigned NOT NULL DEFAULT '0',
+    `variable_type` varchar(20) NOT NULL,
+    `page_id` bigint unsigned DEFAULT NULL,
+    PRIMARY KEY (`variable_id`),
+    KEY `PAGE_VARIABLE_FK01` (`page_id`),
+    CONSTRAINT `PAGE_VARIABLE_FK01` FOREIGN KEY (`page_id`) REFERENCES `page` (`page_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- `kube-watcher`.chart_query definition
+
+CREATE TABLE `chart_query` (
+    `c_query_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+    `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `api_query` varchar(1000) NOT NULL,
+    `legend` varchar(100) NOT NULL,
+    `query_step` varchar(3) NOT NULL,
+    `query_type` varchar(10) NOT NULL DEFAULT 'METRIC',
+    `panel_id` bigint unsigned DEFAULT NULL,
+    PRIMARY KEY (`c_query_id`),
+    KEY `CHART_QUERY_FK01` (`panel_id`),
+    CONSTRAINT `CHART_QUERY_FK01` FOREIGN KEY (`panel_id`) REFERENCES `page_row_panel` (`panel_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
