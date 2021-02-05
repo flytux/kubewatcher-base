7477397e1831960c87bd7667ac59e2d2d674815f
Bs-yoo <dbqudtjd1122@gmail.com>
2021-02-05 10:11:26 +0900

    1. getPodLog Map 생성방식 변경

--- src/main/java/com/kubeworks/watcher/ecosystem/kubernetes/service/impl/PodLogsServiceImpl.java
+++ src/main/java/com/kubeworks/watcher/ecosystem/kubernetes/service/impl/PodLogsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.kubeworks.watcher.ecosystem.kubernetes.service.impl;
 
+import com.google.common.collect.ImmutableMap;
 import com.kubeworks.watcher.ecosystem.kubernetes.handler.CoreV1ApiExtendHandler;
 import com.kubeworks.watcher.ecosystem.kubernetes.service.PodLogsService;
 import io.kubernetes.client.openapi.ApiClient;
@@ -11,7 +12,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
@@ -22,53 +22,47 @@ public class PodLogsServiceImpl implements PodLogsService  {
 
     private final ApiClient k8sApiClient;
     private final CoreV1ApiExtendHandler coreApi;
-    private final Map<String, String> logMap;
 
     public PodLogsServiceImpl(ApiClient k8sApiClient) {
         this.k8sApiClient = k8sApiClient;
         this.coreApi = new CoreV1ApiExtendHandler(k8sApiClient);
-        this.logMap = new HashMap<>();
     }
 
     /**
      * @link(https://v1-18.docs.kubernetes.io/docs/reference/generated/kubernetes-api/v1.18/#read-log-pod-v1-core)
      */
-//    @SneakyThrows
-//    @Override
-//    public String getPodLog(String podName, String namespace, String container) throws ApiException {
-//        String log = coreApi.readNamespacedPodLog(podName, namespace, container,false,false,
-//            null, "true",false,null,null, true);
-//            return log;
-//    }
 
     @SneakyThrows
     @Override
     public Map<String, String> getPodLog(String podName, String namespace, String container, String sinceTime) throws ApiException {
-        Optional<String> aTrue = Optional.ofNullable(coreApi.readNamespacedPodLog(podName, namespace, container, false, false,
+        Optional<String> podLogOptional = Optional.ofNullable(coreApi.readNamespacedPodLog(podName, namespace, container, false, false,
             null, "true", false, null, sinceTime, null, true));
 
-        String lastLogTime = "";
-        if (aTrue.isPresent()) {
-            String s = aTrue.get();
-            lastLogTime = s.substring(
-                StringUtils.lastIndexOf(s, "\n", s.length() - 2) + 1,
-                StringUtils.indexOf(s, "Z", StringUtils.lastIndexOf(s, "\n", s.length() - 2)) + 1
-            );
-
-            lastLogTime = ZonedDateTime.parse(lastLogTime)
-                .truncatedTo(ChronoUnit.SECONDS)
-                .plusSeconds(1)
-                .toString();
-        } else {
-            lastLogTime = sinceTime;
+        ImmutableMap.Builder<String, String> responseBuilder = ImmutableMap.builder();
+        if (!podLogOptional.isPresent()) {
+            return responseBuilder
+                .put("log", "")
+                .put("lastLogTime", sinceTime)
+                .build();
         }
 
-        logMap.put("log", aTrue.orElse(""));
-        logMap.put("lastLogTime", lastLogTime);
+        String logString = podLogOptional.get();
+        String lastLogTime = getLastLogTime(logString);
+        lastLogTime = ZonedDateTime.parse(lastLogTime).truncatedTo(ChronoUnit.SECONDS)
+            .plusSeconds(1).toString();
 
-        return logMap;
+        return responseBuilder
+            .put("log", podLogOptional.get())
+            .put("lastLogTime", lastLogTime)
+            .build();
     }
 
+    private String getLastLogTime(String logString) {
+        return logString.substring(
+            StringUtils.lastIndexOf(logString, "\n", logString.length() - 2) + 1,
+            StringUtils.indexOf(logString, "Z", StringUtils.lastIndexOf(logString, "\n", logString.length() - 2)) + 1
+        );
+    }
 
 
 }
