4cbfafc39132fc990d1d8112ac4e59dad30c2cbb
onlystone <onlystone@gmail.com>
2021-02-05 17:52:22 +0900

    alert 기능 로칼 커밋

---
+++ src/main/java/com/kubeworks/watcher/alert/rule/config/service/AlertRuleConfigSerivceImpl.java
@@ -0,0 +1,25 @@
+package com.kubeworks.watcher.alert.rule.config.service;
+
+import com.kubeworks.watcher.alert.rule.config.AlertRuleConfigService;
+import com.kubeworks.watcher.data.entity.KwAlertRule;
+import com.kubeworks.watcher.data.repository.KwAlertRuleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class AlertRuleConfigSerivceImpl implements AlertRuleConfigService {
+    private final KwAlertRuleRepository kwAlertRuleRepository;
+
+    @Autowired
+    public AlertRuleConfigSerivceImpl(KwAlertRuleRepository kwAlertRuleRepository) {
+        this.kwAlertRuleRepository = kwAlertRuleRepository;
+    }
+
+    public List<KwAlertRule> getAlertRuleList() {
+        return kwAlertRuleRepository.findAllBy();
+    }
+}
