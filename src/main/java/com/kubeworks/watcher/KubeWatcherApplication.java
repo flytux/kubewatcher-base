0c82eebf3327a7061ce2a47f9f5a14436975d873
llooniverse <llooniverse@gmail.com>
2021-08-20 10:45:06 +0900

    Graceful shutdown 설정 PID 파일 관련 설정 & kill.sh 추가

--- src/main/java/com/kubeworks/watcher/KubeWatcherApplication.java
+++ src/main/java/com/kubeworks/watcher/KubeWatcherApplication.java
@@ -1,12 +1,13 @@
 package com.kubeworks.watcher;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class KubeWatcherApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(KubeWatcherApplication.class, args);
+    public static void main(final String[] args) {
+        new SpringApplicationBuilder(KubeWatcherApplication.class).listeners(new ApplicationPidFileWriter()).run(args);
     }
 }
